d8c8884812b25b13a95b650d06d7c0621c702d64
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-20 02:01:06 +0530

    error handling using router dom

---
+++ src/components/Error.js
@@ -0,0 +1,18 @@
+import { useRouteError } from "react-router-dom";
+//this helps to manage errors efficiently 
+
+const Error = ()=>{
+    const err = useRouteError();
+    //this return a detailed description of the error
+    console.log(err);
+    
+    return(
+        <>
+        <h1>Oops! something went wrong</h1>
+        <h2>Check the URL again</h2>
+        <h3>Status:{err.status}</h3>
+        <h4>{err.statusText}</h4>
+        </>
+    )
+}
+export default Error;
