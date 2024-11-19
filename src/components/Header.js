f9e22fff11c4a3361641bb9244493ea154b5fd4d
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-20 02:01:29 +0530

    pages linked using LINK

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import { LOGO_URL } from "../utils/contants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const [buttonState, setbuttonState]= useState("login");
-    // console.log("header renderd");   
+    console.log("header renderd");   
+
+
+    useEffect(()=>{
+        console.log("useEffect called");
+        
+    },[buttonState])
+    //everytime buttonState changes it will be called 
     
 
     return (
@@ -13,10 +21,10 @@ const Header = () => {
             </div>
             <div id="navbar-container">
                 <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">Food</a></li>
+
+                <   li><Link to="/">Home</Link></li>                      {/* if you use anchor tags to reroute you they refresh the entire page so we use LINK to */}
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>                    <li><a href="">Food</a></li>
                     {/* <button onClick={()=>{setbuttonState ("logout")}}>{buttonState}</button> */}
                     {/* as soon as you clicked on the button react updated the buttonState from login to logout and runs the diff algorithm and also refreshes the header. You might think that it just  renders the button but no the whole header component is rendered again. And once it is rerenderd it calculates the diff btw old dom and new dom and it sees that only the button got changed so it updates the button  */}
                     <button onClick={()=>{
