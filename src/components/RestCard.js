d0f174304ce9122bde4bca7683db0545697cc895
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-19 10:08:50 +0530

    shimmer ui created

--- src/components/RestCard.js
+++ src/components/RestCard.js
@@ -3,10 +3,10 @@ import { CDN_URL } from "../utils/contants";
 const RestCard = (props) => {
     //arguments that are passed react will wrap them as object and will be called as props so props are objects and we can access object properties using dot notation to dynamically enter data 
     // console.log(props);
-    console.log(props); //this contain the resInfo object 
-    console.log(props.resInfo); //this line and 981 logs the same thing that we've taken the reInfo object from the props aobject and stored it in resInfo var
+    // console.log(props); //this contain the resInfo object 
+    // console.log(props.resInfo); //this line and 981 logs the same thing that we've taken the reInfo object from the props aobject and stored it in resInfo var
     const {resInfo} = props;
-    console.log(resInfo);
+    // console.log(resInfo);
     
     const {name, locality, costForTwo, cloudinaryImageId, avgRating} = resInfo?.info
     
