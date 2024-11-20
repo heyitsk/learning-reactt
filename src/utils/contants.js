a79885aa26f2e4493cb04ea7542448b3fc0b1689
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-20 17:17:55 +0530

    Menu api url added

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -3,3 +3,6 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 
 export const LOGO_URL = "https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg";
+
+
+export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId=";
